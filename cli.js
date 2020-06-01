5b47f47fe2a66418757058919b0499e7b1afea34
b1conrad <bruce_conrad@byu.edu>
2020-06-01 17:43:36 -0600

    first workable version

--- cli.js
+++ cli.js
@@ -1,7 +1,22 @@
 #!/usr/bin/env node
-
 const [,, ...args] = process.argv
+const prompt = require('prompt')
+const request = require('request')
 
-//let name = prompt("What is your name?")
+prompt.start()
 console.log(`Hello world of ${args}`)
+let question = "What is your query?"
+prompt.get(question, function(err,result) {
+  if (err) { console.log(err) }
+  console.log(`Your query is ${result[question]}`)
+  request.get('http://localhost:8080'+result[question],
+  function(error,response){
+    if(error) {console.log('Error:',error)}
+    if (!error && response.statusCode == 200) {
+      console.log(response.headers['content-type'])
+      console.log(JSON.stringify(response.body))
+      console.log(JSON.parse(response.body))
+    }
+  })
+})
 
