35c2ed92e8c7c933d1accf47e296bfa57947044c
b1conrad <bruce_conrad@byu.edu>
2020-06-02 17:31:10 -0600

    better error reporting

--- cli.js
+++ cli.js
@@ -11,11 +11,23 @@ prompt.get(question, function(err,result) {
   console.log(`Your query is ${result[question]}`)
   request.get('http://localhost:8080'+result[question],
   function(error,response){
-    if(error) {console.log('Error:',error)}
-    if (!error && response.statusCode == 200) {
-      console.log(response.headers['content-type'])
-      console.log(JSON.stringify(response.body))
-      console.log(JSON.parse(response.body))
+    if(error) {console.log('Error:',error); return null}
+    //console.log(JSON.stringify(response,null,2))
+    if (response.statusCode == 200) {
+      //console.log(response.headers['content-type'])
+      if (/^application\/json;/.test(response.headers['content-type'])) {
+        console.log(JSON.parse(response.body))
+      } else {
+        console.log(response.body)
+      }
+    } else {
+      var msg = 'unknown error'
+      if (/"error":"([^"]*)"/.test(response.body)) {
+        msg = /"error":"([^"]*)"/.exec(response.body)[1]
+      } else if (/<pre>([^<]*)<\/pre>/.test(response.body)) {
+        msg = /<pre>([^<]*)<\/pre>/.exec(response.body)[1]
+      }
+      console.log(response.statusCode,msg)
     }
   })
 })
