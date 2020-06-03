6d56bc0cf2e96b45f4a2b9dc1f8cbccba74681af
b1conrad <bruce_conrad@byu.edu>
2020-06-03 09:53:22 -0600

    multiple queries

--- cli.js
+++ cli.js
@@ -5,30 +5,45 @@ const request = require('request')
 
 prompt.start()
 console.log(`Hello world of ${args}`)
-let question = "What is your query?"
-prompt.get(question, function(err,result) {
-  if (err) { console.log(err) }
-  console.log(`Your query is ${result[question]}`)
-  request.get('http://localhost:8080'+result[question],
-  function(error,response){
-    if(error) {console.log('Error:',error); return null}
-    //console.log(JSON.stringify(response,null,2))
-    if (response.statusCode == 200) {
-      //console.log(response.headers['content-type'])
-      if (/^application\/json;/.test(response.headers['content-type'])) {
-        console.log(JSON.parse(response.body))
-      } else {
-        console.log(response.body)
-      }
-    } else {
-      var msg = 'unknown error'
-      if (/"error":"([^"]*)"/.test(response.body)) {
-        msg = /"error":"([^"]*)"/.exec(response.body)[1]
-      } else if (/<pre>([^<]*)<\/pre>/.test(response.body)) {
-        msg = /<pre>([^<]*)<\/pre>/.exec(response.body)[1]
-      }
-      console.log(response.statusCode,msg)
-    }
-  })
-})
 
+function prompt2 (schema) {
+  return new Promise(function (resolve, reject) {
+    prompt.get(schema, function (err, result) {
+      return err ? reject(err) : resolve(result)
+    })
+  })
+}
+async function main () {
+  while (true) {
+    let question = 'What is your query?'
+    let result = await prompt2(question)
+    console.log(`Your query is ${result[question]}`)
+    if (result[question] === 'exit') {
+      break
+    }
+    request.get('http://localhost:8080'+result[question],
+    function(error,response){
+      if(error) {console.log('Error:',error); return null}
+      //console.log(JSON.stringify(response,null,2))
+      if (response.statusCode == 200) {
+        //console.log(response.headers['content-type'])
+        if (/^application\/json;/.test(response.headers['content-type'])) {
+          console.log(JSON.parse(response.body))
+        } else {
+          console.log(response.body)
+        }
+      } else {
+        var msg = 'unknown error'
+        if (/"error":"([^"]*)"/.test(response.body)) {
+          msg = /"error":"([^"]*)"/.exec(response.body)[1]
+        } else if (/<pre>([^<]*)<\/pre>/.test(response.body)) {
+          msg = /<pre>([^<]*)<\/pre>/.exec(response.body)[1]
+        }
+        console.log(response.statusCode,msg)
+      }
+    })
+  }
+  console.log('bye!')
+}
+
+main().catch(console.error)
