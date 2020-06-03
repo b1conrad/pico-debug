aafccec499ea62c9dd282dce5f6874bda1988a36
b1conrad <bruce_conrad@byu.edu>
2020-06-03 11:21:22 -0600

    use node-fetch

--- cli.js
+++ cli.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 const [,, ...args] = process.argv
 const prompt = require('prompt')
-const request = require('request')
+const fetch = require('node-fetch')
 
 prompt.start()
-console.log(`Hello world of ${args}`)
 
 function prompt2 (schema) {
   return new Promise(function (resolve, reject) {
@@ -14,34 +13,35 @@ function prompt2 (schema) {
   })
 }
 async function main () {
+  console.log(`Hello world of ${args}`)
   while (true) {
     let question = 'What is your query?'
     let result = await prompt2(question)
-    console.log(`Your query is ${result[question]}`)
     if (result[question] === 'exit') {
       break
     }
-    request.get('http://localhost:8080'+result[question],
-    function(error,response){
-      if(error) {console.log('Error:',error); return null}
-      //console.log(JSON.stringify(response,null,2))
-      if (response.statusCode == 200) {
-        //console.log(response.headers['content-type'])
-        if (/^application\/json;/.test(response.headers['content-type'])) {
-          console.log(JSON.parse(response.body))
-        } else {
-          console.log(response.body)
-        }
+    console.log(`Your query is ${result[question]}`)
+    let response = await fetch('http://localhost:8080'+result[question])
+    //console.log(JSON.stringify(response,null,2))
+    if (response.status == 200) {
+      console.log(response.headers.get('content-type'))
+      if (/^application\/json;/.test(response.headers.get('content-type'))) {
+        let json = await response.json()
+        console.log(json)
       } else {
-        var msg = 'unknown error'
-        if (/"error":"([^"]*)"/.test(response.body)) {
-          msg = /"error":"([^"]*)"/.exec(response.body)[1]
-        } else if (/<pre>([^<]*)<\/pre>/.test(response.body)) {
-          msg = /<pre>([^<]*)<\/pre>/.exec(response.body)[1]
-        }
-        console.log(response.statusCode,msg)
+        let body = await response.text()
+        console.log(body)
       }
-    })
+    } else {
+      var msg = 'unknown error'
+      body = await response.text()
+      if (/"error":"([^"]*)"/.test(body)) {
+        msg = /"error":"([^"]*)"/.exec(body)[1]
+      } else if (/<pre>([^<]*)<\/pre>/.test(body)) {
+        msg = /<pre>([^<]*)<\/pre>/.exec(body)[1]
+      }
+      console.log(response.status,msg)
+    }
   }
   console.log('bye!')
 }
